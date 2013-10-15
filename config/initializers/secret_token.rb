27489c10d2b2bc29d1b6e8c18ae9652e33757c0c
Ryan Rogers <ryansrogers85@gmail.com>
2013-10-15 10:29:53 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpsDash::Application.config.secret_token = '195e22b28f0ddc7c2d1bffa6d14a49fd44c62a5b26cbd230801a637c6960b4c7348fe64c33e9673d1356caaf76e2515fa6dd5ca3720b8d8d8c791490c5eeb149'
