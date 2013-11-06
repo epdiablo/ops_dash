b255bfb3a2f74aecce68e52aeb7875808dced246
Ryan Rogers <ryansrogers85@gmail.com>
2013-11-05 23:16:43 -0500

    refactor ticket form

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,4 +2,7 @@ class Ticket < ActiveRecord::Base
   belongs_to :user
   has_many :updates
   attr_accessible :description, :issue, :priority, :category, :user_id, :status
+  
+  PRIORITY_LEVELS = [ 1, 2, 3]
+  
 end
