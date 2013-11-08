bdc8c52da7a83cbb876f13ed3114289b74f60147
Ryan Rogers <rrogers@songza.com>
2013-11-08 16:49:37 -0500

    added in owner to db and view

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,7 +1,7 @@
 class Ticket < ActiveRecord::Base
   belongs_to :user
   has_many :updates
-  attr_accessible :description, :issue, :priority, :category, :user_id, :status
+  attr_accessible :description, :issue, :priority, :category, :user_id, :status, :owner
   
   PRIORITY_LEVELS = [ 1, 2, 3]
   
