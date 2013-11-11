8da01c174722267f27f1a6a070b06e88910f0c51
Ryan Rogers <ryansrogers85@gmail.com>
2013-11-11 02:55:02 -0500

    assets precompile

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,6 @@ module OpsDash
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.precompile += [/^[-_a-zA-Z0-9]*\..*/]
   end
 end
