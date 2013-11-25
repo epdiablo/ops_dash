4d2fa8bba243dda82570054b479403028e15ef22
Ryan Rogers <ryansrogers85@gmail.com>
2013-11-25 17:33:30 -0500

    watchers

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(:version => 20131113215237) do
   create_table "tickets", :force => true do |t|
     t.integer  "user_id"
     t.text     "description", :limit => 255
-    t.string   "issue"
+    t.text     "issue",       :limit => 255
     t.integer  "priority"
     t.string   "category"
     t.datetime "created_at",                 :null => false
@@ -44,11 +44,11 @@ ActiveRecord::Schema.define(:version => 20131113215237) do
   end
 
   create_table "updates", :force => true do |t|
-    t.string   "body"
+    t.text     "body",       :limit => 255
     t.string   "category"
     t.integer  "ticket_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",                :null => false
+    t.datetime "updated_at",                :null => false
     t.integer  "user_id"
     t.boolean  "is_closer"
   end
